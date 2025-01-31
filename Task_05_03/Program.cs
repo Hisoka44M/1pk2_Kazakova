84648746203f04dbc8da4d414cb2e64374899bea
his44 <148785343+his44@users.noreply.github.com>
2025-01-31 22:11:09 +0500

    изменения

---
+++ Task_05_03/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Task_05_03
+{
+    internal class Program
+    {
+        //Даны два массива, заполненные символами английского алфавита размером 3*3. Проверить, являются ли матрицы равными, если 
+        //да, то вывести сообщение о том, что они равны, если нет, то вывести повторно матрицы с цветовой индикацией только тех
+        //элементов, которые равны. (матрицы считаются равными, если их соответствующие элементы равны.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Не совпадающие символы выделаются синим");
+            string[,] arr1 = new string[3, 3];
+            string[,] arr2 = new string[3, 3];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arr1[i, j] = ((char)rnd.Next('A', 'Z' + 1)).ToString();
+                    arr2[i, j] = ((char)rnd.Next('A', 'Z' + 1)).ToString();
+                }
+            }
+
+            bool equal = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr1[i, j] != arr2[i, j])
+                    {
+                        equal = false;
+                    }
+                }
+            }
+
+            if (equal)
+            {
+                Console.WriteLine("\nМатрицы равны");
+            }
+            else
+            {
+                Console.WriteLine("\nМатрицы не равны");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for(int j = 0; j < 3; j++)
+                    {
+                        if (arr1[i,j] == arr2[i,j])
+                        {
+                            Console.Write(arr1[i,j] + " ");
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.Write(" " + arr1[i, j] + " "); 
+                            Console.ResetColor();
+                        }
+                    }
+                    Console.Write(" | ");
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (arr1[i, j] == arr2[i, j])
+                        {
+                            Console.Write(arr2[i, j] + " ");
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.Write(" " + arr2[i, j] + " "); 
+                            Console.ResetColor();
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Task_05_05/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Task_05_05
+{
+    internal class Program
+    {
+        //*У пользователя в консоли запрашивается числа n и m, генерируется прямоугольный массив целых числе [n*m]. Заполнение 
+        //случайными числами в диапазоне от -99 до 99 включительно.Осуществите без создания нового массива преобразование
+        //текущего по следующему правилу: 
+        //• Если элемент меньше нуля, то отбрасываем знак и выделяем при выводе зеленым цветом 
+        //• Если элемент равен нулю, то перезаписываем единицу и выделяем при выводе красным цветом 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сколько будет строк в массиве");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сколько будет столбцов в массиве");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[n, m];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = rnd.Next(-99, 100);
+                }
+            }
+
+            for (int i = 0;i < n; i++)
+            { 
+                for (int j = 0;j < m; j++)
+                {
+                    if (arr[i, j] < 0)
+                    {
+                        arr[i, j] = Math.Abs(arr[i, j]);
+                    }
+                    else if (arr[i, j] == 0)
+                    {
+                        arr[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (arr[i, j] > 0 && arr[i, j] <= 99) 
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else if (arr[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+
+                    Console.Write(arr[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
